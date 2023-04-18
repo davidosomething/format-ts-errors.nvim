0e8b5893c7b1640de44d8dd6afeb554012141e70
David O'Trakoun <me@davidosomething.com>
2023-04-18 18:10:02 -0400

    support ts error 2741

--- lua/format-ts-errors/init.lua
+++ lua/format-ts-errors/init.lua
@@ -1,32 +1,61 @@
 local M = {}
 
-M[2322] = function (msg)
+M.format_object_type = function(o)
+  o = vim.fn.substitute(o, "; ", ";", "g")
+  o = vim.fn.substitute(o, ";", ";\n", "g")
+  o = vim.fn.substitute(o, "{ ", "{\n", "g")
+
+  -- indent
+  o = vim.fn.split(o, "\n")
+  local obj = ""
+  local level = 0
+  for _, line in ipairs(o) do
+    if line:find("}") then
+      level = level - 1
+    end
+    local spaces = ("  "):rep(level)
+    obj = obj .. spaces .. line .. "\n"
+    if line:find("{") then
+      level = level + 1
+    end
+  end
+  return obj
+end
+
+M[2322] = function(msg)
   -- "Object literal may only specify known properties, and 'third' does not exist in type '{ second: { str: string; int: number; }; }'."
   ---@diagnostic disable-next-line: unused-local
   local _start, _end, key, deep =
     msg:find("Object.*and '(.-)' does not exist in type '(.*)'.")
+
+  msg:find("Object.*and '(.-)' does not exist in type '(.*)'.")
   if key and deep then
-    deep = vim.fn.substitute(deep, "; ", ";", "g")
-    deep = vim.fn.substitute(deep, ";", ";\n", "g")
-    deep = vim.fn.substitute(deep, "{ ", "{\n", "g")
-    deep = vim.fn.split(deep, "\n")
-    local obj = ""
-    local level = 0
-    for _, line in ipairs(deep) do
-      if line:find("}") then
-        level = level - 1
-      end
-      local spaces = ("  "):rep(level)
-      obj = obj .. spaces .. line .. "\n"
-      if line:find("{") then
-        level = level + 1
-      end
-    end
+    local obj = M.format_object_type(deep)
     return (
       "Object literal may only specify known properties, and"
       .. ("\n\n  %s"):format(key)
       .. "\n\ndoes not exist in type"
-      .. ("\n\n%s\n"):format(obj)
+      .. ("\n\n%s"):format(obj)
+      .. "\n"
+    )
+  end
+  return msg
+end
+
+M[2741] = function(msg)
+  -- Property 'first' is missing in type '{}' but required in type 'Deep'.
+  ---@diagnostic disable-next-line: unused-local
+  local _start, _end, needle, a, b =
+    msg:find("Property '(.-)' is missing in type '(.-)' but required in type '(.*)'.")
+  if needle and a and b then
+    return (
+      "Property"
+      .. ("\n\n  %s"):format(needle)
+      .. "\n\nis missing in type"
+      .. ("\n\n%s"):format(M.format_object_type(a))
+      .. "\nbut required in type"
+      .. ("\n\n%s"):format(M.format_object_type(b))
+      .. "\n"
     )
   end
   return msg
